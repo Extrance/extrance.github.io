5453551bee5e1f1407710912c17fa368b1b2af3d
Extrance <balasso.andrea69@gmail.com>
2024-08-14 17:24:07 +0200

    Update SpeedDial

--- src/components/common/custom/SpeedDial.jsx
+++ src/components/common/custom/SpeedDial.jsx
@@ -1,19 +1,21 @@
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
-import { Backdrop, Box, SpeedDial as Dial, SpeedDialAction, SpeedDialIcon, useTheme } from '@mui/material';
+import { Backdrop, SpeedDial as Dial, SpeedDialAction, SpeedDialIcon, useTheme } from '@mui/material';
 import { useState } from 'react';
 import { isMobile } from 'react-device-detect';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import PayPalIcon from '../../UI/Icons/PayPalIcon';
 
 const linkedInUrl = "http://www.linkedin.com/in/andreabalasso97";
 const instagramUrl = "https://www.instagram.com/extranceit";
+const youtubeUrl = "https://www.youtube.com/@extranceIT";
+const paypalUrl = "https://www.paypal.me/extrance";
 
 const actions = [
   { icon: <LinkedInIcon />, name: 'LinkedIn', onClick: () => window.open(linkedInUrl, "_blank") },
   { icon: <InstagramIcon />, name: 'Instagram', onClick: () => window.open(instagramUrl, "_blank") },
+  { icon: <YouTubeIcon />, name: 'YouTube', onClick: () => window.open(youtubeUrl, "_blank") },
+  { icon: <PayPalIcon />, name: 'PayPal', onClick: () => window.open(paypalUrl, "_blank") },
 ];
 
 export default function SpeedDial() {
@@ -26,7 +28,7 @@ export default function SpeedDial() {
     <>
       <Backdrop open={open} />
       <Dial
-        ariaLabel="SpeedDial tooltip example"
+        ariaLabel="speed-dial"
         sx={{ position: 'fixed', bottom: 16, right: 16 }}
         icon={<SpeedDialIcon sx={{ color: theme.palette.mode === "dark" ? "black" : "white" }} />}
         onClose={handleClose}
